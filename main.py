0bc299ae01f70dbde5aba164136b874f14f5f096
Idowu102 <86113735+Idowu102@users.noreply.github.com>
2026-05-16 22:08:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -34,6 +34,9 @@ def base_opts():
         "geo_bypass": True,
         "socket_timeout": 60,
 
+        # USE COOKIES
+        "cookiefile": "cookies.txt",
+
         "extractor_args": {
             "youtube": {
                 "player_client": ["android", "web"]
